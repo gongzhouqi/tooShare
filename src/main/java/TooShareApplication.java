24f4e175aa92e5850e8353cf85e1a716e36b01a7
Zhouqi 30L <gongzhouqi@gmail.com>
2022-02-10 22:31:04 -0500

    add author

--- src/main/java/TooShareApplication.java
+++ src/main/java/TooShareApplication.java
@@ -1,3 +1,9 @@
+/*
+ * @Author Zhouqi Gong
+ */
+
+
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
